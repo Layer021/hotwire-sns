e2769ec95e21a43ba54635afe673eb7ed1be5fef
Takashi Yamamoto <taka.layer.021@gmail.com>
2025-03-20 18:25:40 +0900

    feat: フォロー処理をTurboStream化

--- app/controllers/user/following_users_controller.rb
+++ app/controllers/user/following_users_controller.rb
@@ -5,17 +5,38 @@ class User::FollowingUsersController < ApplicationController
 
   def create
     current_user.follow(@user)
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.turbo_stream { render_turbo_stream }
+    end
   end
 
   def destroy
     current_user.unfollow(@user)
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.turbo_stream { render_turbo_stream }
+    end
   end
 
   private
 
     def find_user
       @user = User.find(params[:id])
+    end
+
+    def render_turbo_stream
+      render turbo_stream: [
+        turbo_stream.replace(@user, partial: "shareds/user", locals: {
+          user: @user,
+          show_follow_button: true,
+          has_been_followed: @user.has_been_followed_by?(current_user)
+        }),
+        turbo_stream.replace(current_user, partial: "shareds/user", locals: {
+          user: current_user,
+          show_follow_button: false,
+          has_been_followed: false
+        })
+      ]
     end
 end
