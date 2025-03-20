b93b4c10daa15bd822323904495bed306f6a9e02
Takashi Yamamoto <taka.layer.021@gmail.com>
2025-03-20 18:01:55 +0900

    feat: いいねをTurboStream化

--- app/controllers/user/liked_posts_controller.rb
+++ app/controllers/user/liked_posts_controller.rb
@@ -5,17 +5,31 @@ class User::LikedPostsController < User::ApplicationController
 
   def create
     current_user.like_post(@post)
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.turbo_stream { render_turbo_stream }
+    end
   end
 
   def destroy
     current_user.unlike_post(@post)
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.turbo_stream { render_turbo_stream }
+    end
   end
 
   private
 
     def find_post
       @post = Post.find(params[:id])
+    end
+
+    def render_turbo_stream
+      render turbo_stream: turbo_stream.replace(@post, partial: "shareds/post", locals: {
+        post: @post,
+        has_been_liked: @post.has_been_liked_by?(current_user),
+        show_delete_button: @post.user == current_user
+      })
     end
 end
