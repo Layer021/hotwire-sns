a54dcd9f7f5b8e969298a661120c906a4cb806cf
Takashi Yamamoto <taka.layer.021@gmail.com>
2025-03-21 12:28:53 +0900

    fix: Seeder修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,10 @@ class Seeder
         model.upsert_all(attrs)
       end
     end
+
+    ActiveRecord::Base.connection.tables.each do |table|
+      ActiveRecord::Base.connection.reset_pk_sequence!(table)
+    end
   end
 
   private
